eb4720954806b8813f76f88f06a8605c2fdf6f4a
cmontella <cmontella@live.com>
2018-05-26 19:02:49 -0700

    Fix eve mention

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -83,11 +83,11 @@ impl ClientHandler {
   let system_timer = Hasher::hash_str("system/timer");
   let ball = Hasher::hash_str("ball");
   runner.program.mech.runtime.register_blocks(vec![position_update(), export_ball()], &mut runner.program.mech.store);
-  let mut balls = make_balls(10);
+  let mut balls = make_balls(10000);
   let mut txn = Transaction::from_changeset(vec![
     Change::NewTable{tag: system_timer, rows: 10, columns: 8}, 
     Change::NewTable{tag: ball, rows: 10, columns: 6}, 
-    Change::Add{table: system_timer, row: 1, column: 1, value: Value::from_u64(1000)},
+    Change::Add{table: system_timer, row: 1, column: 1, value: Value::from_u64(16)},
   ]); 
   let txn2 = Transaction::from_changeset(balls);
   outgoing.send(RunLoopMessage::Transaction(txn));
@@ -213,7 +213,7 @@ fn main() {
       .short("p")
       .long("port")
       .value_name("PORT")
-      .help("Sets the port for the Eve server (3012)")
+      .help("Sets the port for the Mech server (3012)")
       .takes_value(true))
     .arg(Arg::with_name("http-port")
       .short("t")
@@ -244,16 +244,16 @@ fn make_balls(n: u64) -> Vec<Change> {
   for i in 0 .. n + 1 {
 
     let mut rng = thread_rng();
-    let x = rng.gen_range(1, 100);
-    let y = rng.gen_range(1, 100);
-    let dx = rng.gen_range(1, 10);
-    let dy = rng.gen_range(1, 10);
+    let x = rng.gen_range(1, 500);
+    let y = rng.gen_range(1, 500);
+    let dx = rng.gen_range(1, 300);
+    let dy = rng.gen_range(1, 100);
     let ball = Hasher::hash_str("ball");
   
     v.push(Change::Add{table: ball, row: i, column: 1, value: Value::from_u64(x)});
     v.push(Change::Add{table: ball, row: i, column: 2, value: Value::from_u64(y)});
     v.push(Change::Add{table: ball, row: i, column: 3, value: Value::from_u64(dx)});
-    v.push(Change::Add{table: ball, row: i, column: 4, value: Value::from_u64(dy)});
+    v.push(Change::Add{table: ball, row: i, column: 4, value: Value::from_u64(0)});
   
   }
   v
@@ -268,24 +268,29 @@ fn position_update() -> Block {
   block.add_constraint(Constraint::Scan {table: ball, column: 2, input: 3});
   block.add_constraint(Constraint::Scan {table: ball, column: 3, input: 4});
   block.add_constraint(Constraint::Scan {table: ball, column: 4, input: 5});  
-  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![5, 6], output: 1}); 
-  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![7, 8], output: 2});
-  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![8, 4], output: 3});
-  block.add_constraint(Constraint::Constant {value: 10, input: 4});
-  block.add_constraint(Constraint::Identity {source: 2, sink: 5});
-  block.add_constraint(Constraint::Identity {source: 4, sink: 6});
-  block.add_constraint(Constraint::Identity {source: 3, sink: 7});
-  block.add_constraint(Constraint::Identity {source: 5, sink: 8});
-  block.add_constraint(Constraint::Insert {output: 1, table: ball, column: 1});
-  block.add_constraint(Constraint::Insert {output: 2, table: ball, column: 2});
-  block.add_constraint(Constraint::Insert {output: 3, table: ball, column: 4});
+  block.add_constraint(Constraint::Identity {source: 2, sink: 1});
+  block.add_constraint(Constraint::Identity {source: 4, sink: 2});
+  block.add_constraint(Constraint::Identity {source: 3, sink: 3});
+  block.add_constraint(Constraint::Identity {source: 5, sink: 4});
+  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![1, 2], output: 5}); 
+  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![3, 4], output: 6});
+  block.add_constraint(Constraint::Constant {value: 1, input: 7});
+  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![4, 7], output: 8});
+  block.add_constraint(Constraint::Insert {output: 5, table: ball, column: 1});
+  block.add_constraint(Constraint::Insert {output: 6, table: ball, column: 2});
+  block.add_constraint(Constraint::Insert {output: 7, table: ball, column: 4});
   let plan = vec![
-    Constraint::Function {operation: Function::Add, parameters: vec![5, 6], output: 1},
-    Constraint::Function {operation: Function::Add, parameters: vec![7, 8], output: 2},
-    Constraint::Function {operation: Function::Add, parameters: vec![8, 4], output: 3},
-    Constraint::Insert {output: 1, table: ball, column: 1},
-    Constraint::Insert {output: 2, table: ball, column: 2},
-    Constraint::Insert {output: 3, table: ball, column: 4},
+    Constraint::Identity {source: 2, sink: 1},
+    Constraint::Identity {source: 4, sink: 2},
+    Constraint::Identity {source: 3, sink: 3},
+    Constraint::Identity {source: 5, sink: 4},
+    Constraint::Constant {value: 10, input: 7},
+    Constraint::Function {operation: Function::Add, parameters: vec![1, 2], output: 5},
+    Constraint::Function {operation: Function::Add, parameters: vec![3, 4], output: 6},
+    Constraint::Function {operation: Function::Add, parameters: vec![4, 7], output: 8},
+    Constraint::Insert {output: 5, table: ball, column: 1},
+    Constraint::Insert {output: 6, table: ball, column: 2},
+    Constraint::Insert {output: 8, table: ball, column: 4},
   ];
   block.plan = plan;
   block
