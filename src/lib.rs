ae6d44ecaffec12a00ca0c757ed27f09c6af2e6a
cmontella <cmontella@live.com>
2018-08-10 11:55:46 -0400

    Add extern crates to lib

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,15 @@ extern crate mech_syntax;
 extern crate time;
 #[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate ws;
+extern crate term_painter;
+extern crate walkdir;
 
 // ## Modules
 
 pub mod program;
 pub mod watchers;
+pub mod client;
