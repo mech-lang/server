263cf88f6780a9d619dadab7b344aaca0a8f5b1d
cmontella <cmontella@live.com>
2018-08-26 22:48:54 -0400

    remove watchdiff

--- src/watchers/mod.rs
+++ src/watchers/mod.rs
@@ -8,27 +8,11 @@ use mech_core::Value;
 
 // ## Watchers
 
-#[derive(Debug)]
-pub struct WatchDiff {
-  pub adds: Vec<(u64, u64, u64, Value)>,
-  pub removes: Vec<(u64, u64, u64, Value)>,
-}
-
-impl WatchDiff {
-  pub fn new() -> WatchDiff  {
-    WatchDiff {
-      adds: Vec::new(),
-      removes: Vec::new(),
-    }
-  }
-}
-
 pub trait Watcher {
   fn get_name(& self) -> String;
   fn get_columns(& self) -> usize;
   fn set_name(&mut self, &str);
-  fn on_diff(&mut self, interner: &mut Interner, diff: WatchDiff);
+  fn process_change(&mut self, change: &Change);
 }
 
 pub mod system;
-pub mod websocket;
